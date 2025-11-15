983c75dd10bb9523967bd856b98358017c0db952
c0nap <conap910@gmail.com>
2025-11-15 17:55:07 -0500

    refactor scripts: src.core

---
+++ src/core/__init__.py
@@ -0,0 +1,5 @@
+# This file can be empty - it just marks the directory as a Python package
+
+# Adding __init__.py to a folder makes it a module
+# Run modules using 'python -m <folder>.<script>' instead of 'python <folder>/<script>.py'
+# Benefit: import scripts across folders
