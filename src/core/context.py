eb6cac6704128909b08cd098cc3484bd64ac1f5c
c0nap <conap910@gmail.com>
2025-11-15 20:03:15 -0500

    session singleton / cleanup
    fixes #6

--- src/core/context.py
+++ src/core/context.py
@@ -2,13 +2,8 @@ from components.connectors import RelationalConnector
 from components.document_storage import DocumentConnector
 from components.fact_storage import GraphConnector
 from components.semantic_web import KnowledgeGraph
-from dotenv import load_dotenv
-import os
 from src.util import all_none
 
-
-# Read environment variables at compile time
-load_dotenv(".env")
 
 
 class Session:
@@ -17,9 +12,8 @@ class Session:
         - This class implements Singleton design, so only one session can be created.
         - However, the session config can still be updated using the normal constructor.
     """
-
-    # TODO: this is bad design ^
     _instance = None
+    _created = False
 
     def __new__(cls, *args, **kwargs):
         """Creates a new session at first access, otherwise uses the existing session."""
@@ -34,6 +28,9 @@ class Session:
             - The document database connector is created normally since mongo is the only supported option.
             - The graph database connector is created normally since neo4j is the only supported option.
         """
+        if Session._created:
+            return  # prevent reinitialization
+        Session._created = True
         ## Enables or disables the components from printing debug info.
         self.verbose = verbose
         ## Stores RDF-compliant semantic triples.
@@ -46,17 +43,6 @@ class Session:
         self.main_graph = KnowledgeGraph("main", self.graph_db)
         # TODO: Split into scene graph, event graph, and social graph.
 
-        # TODO: Do not interact with these directly, provide them to EAV Model and Knowledge Graph classes
-
-    def test_database_connections(self):
-        """Configure the databases and verify they are working correctly."""
-        self.relational_db.test_connection()
-        self.docs_db.test_connection()
-        self.graph_db.test_connection()
-
-    def reset(self):
-        """Deletes all created databases and tables."""
-        # TODO
 
 
 if __name__ == "__main__":
