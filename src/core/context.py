31f2d90fa8d7588c49ae9b9b1194e890c853bf34
c0nap <conap910@gmail.com>
2025-11-15 20:16:28 -0500

    session generic args & typing

--- src/core/context.py
+++ src/core/context.py
@@ -5,23 +5,24 @@ from components.semantic_web import KnowledgeGraph
 from src.util import all_none
 
 
-
 class Session:
     """Stores active database connections and configuration settings.
     @details
-        - This class implements Singleton design, so only one session can be created.
-        - However, the session config can still be updated using the normal constructor.
+        - This class implements Singleton design so only one session can be created.
     """
     _instance = None
     _created = False
 
-    def __new__(cls, *args, **kwargs):
-        """Creates a new session at first access, otherwise uses the existing session."""
+    def __new__(cls, *args: Any, **kwargs: Any) -> Session:
+        """Creates a new session at first access, otherwise returns the existing session.
+        @param *args  Positional arguments forwarded to __init__().
+        @param **kwargs  Keyword arguments forwarded to __init__().
+        @return  The new global Session singleton."""
         if cls._instance is None:
             cls._instance = super().__new__(cls)
         return cls._instance
 
-    def __init__(self, verbose=False):
+    def __init__(self, verbose: bool = False) -> None:
         """Initializes the session using the .env file.
         @details
             - The relational database connector is created using a Factory Method, choosing mysql or postgres based on the .env file.
@@ -47,12 +48,17 @@ class Session:
 # Global storage for the lazy singleton
 _session_instance = None
 
-def get_session(verbose: bool = False) -> Session:
-    """ """
+def get_session(*args: Any, **kwargs) -> Session:
+    """Lazily creates a session on first call, otherwise returns the existing session.
+    @note  Will ignore any arguments passed after creation.
+    @param *args  Positional arguments forwarded to Session().
+    @param **kwargs  Keyword arguments forwarded to Session().
+    @return  The global instance of the Session class."""
     global _session_instance
     if _session_instance is None:
-        _session_instance = Session(verbose=verbose)
+        _session_instance = Session(*args, **kwargs)
     return _session_instance
+
 
 
 
