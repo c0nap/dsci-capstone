d3ce16638b8bb897e01fed23d9bd42574d70196e
c0nap <conap910@gmail.com>
2025-12-01 13:45:22 -0500

    optional_param docstring

--- conftest.py
+++ conftest.py
@@ -17,7 +17,10 @@ def pytest_addoption(parser: Any) -> None:
 
 
 def optional_param(name: str, package: str) -> pytest.param:
-    """Return a pytest.param that is skipped if the given package is missing."""
+    """Return a pytest.param that is skipped if the given package is missing.
+    @param name  The fixture name to include in the parameter list.
+    @param package  The name of a Python package to check for.
+    @return  PyTest parameter with the skip flag set if package is not installed."""
     exists = importlib.util.find_spec(package) is not None
     return pytest.param(
         name,
