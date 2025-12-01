4182c6eb517c351a19bfd8e70de5e0c59e034437
c0nap <conap910@gmail.com>
2025-12-01 02:57:04 -0500

    temp patch for #72
    - todo: dummy handling must be re-thought

--- src/connectors/graph.py
+++ src/connectors/graph.py
@@ -84,7 +84,7 @@ class GraphConnector(DatabaseConnector):
             try:  # Create nodes, insert dummy data, and use get_dataframe
                 tmp_kg = "test_graph"
                 with self.temp_graph(tmp_kg):
-                    query = f"MATCH (n:TestPerson {self.SAME_DB_KG_()}) WHERE {self.NOT_DUMMY_()} DETACH DELETE n"
+                    query = f"MATCH (n:TestPerson {self.SAME_DB_KG_()}) DETACH DELETE n"
                     self.execute_query(query, _filter_results=False)
                     query = f"""CREATE (n1:TestPerson {{kg: '{self._graph_name}', name: 'Alice', age: 30}})
                                 CREATE (n2:TestPerson {{kg: '{self._graph_name}', name: 'Bob', age: 25}}) RETURN n1, n2"""
@@ -93,7 +93,7 @@ class GraphConnector(DatabaseConnector):
                     if df is None:
                         raise Log.Failure(Log.gr_db + Log.test_ops + Log.test_df, Log.msg_none_df("graph", {tmp_kg}))
                     check_values([len(df)], [2], self.verbose, Log.gr_db, raise_error=True)
-                    query = f"MATCH (n:TestPerson {self.SAME_DB_KG_()}) WHERE {self.NOT_DUMMY_()} DETACH DELETE n"
+                    query = f"MATCH (n:TestPerson {self.SAME_DB_KG_()}) DETACH DELETE n"
                     self.execute_query(query, _filter_results=False)
             except Exception as e:
                 raise Log.Failure(Log.gr_db + Log.test_ops + Log.test_df, Log.msg_unknown_error) from e
@@ -282,15 +282,12 @@ class GraphConnector(DatabaseConnector):
             # UNION automatically handles duplicates, and we only RETURN n or r, never m.
             query = f"""
             MATCH (n {self.SAME_DB_KG_()})
-            WHERE {self.NOT_DUMMY_('n')}
             RETURN n AS element
             UNION
             MATCH (n {self.SAME_DB_KG_()})-[r]->(m)
-            WHERE {self.NOT_DUMMY_('n')}
             RETURN r AS element
             UNION
             MATCH (n)-[r]->(m {self.SAME_DB_KG_()})
-            WHERE {self.NOT_DUMMY_('m')}
             RETURN r AS element
             """
         df = self.execute_query(query)
