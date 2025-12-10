1d2673d9f6ade7a32f5bc2a297b04907527a3848
c0nap <conap910@gmail.com>
2025-12-10 00:07:47 -0500

    qol shell script additions

--- docker/scripts/run_capstone.sh
+++ docker/scripts/run_capstone.sh
@@ -4,6 +4,10 @@ set -e
 # Start Docker without needing a password
 sudo service docker start
 
+# Clear old results (TMP)
+sleep 3
+docker rm container-python
+
 # Move into your project directory
 cd /mnt/c/dsci-cap/capstone
 
@@ -14,7 +18,7 @@ source ../env-cap/bin/activate
 make docker-workers-dev
 make docker-all-dbs
 make docker-blazor-silent
-make docker-python-dev
+make docker-python-dev 2>&1 | tee ./logs/output.txt
 
 # Extract results from container
 make docker-copy-logs
